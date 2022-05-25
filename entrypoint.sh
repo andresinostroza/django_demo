9ff4be2515072b104a21522d558dd8ed75240204
personal <andresinostrozavalenzuela@gmail.com>
2022-05-25 17:27:45 +0200

    adding basic testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 python manage.py makemigrations api || exit 1
 python manage.py migrate || exit 1
+python manage.py test || exit 1
 python manage.py loaddata organization
 python manage.py initsuperuser
 python manage.py runserver 0.0.0.0:8090
