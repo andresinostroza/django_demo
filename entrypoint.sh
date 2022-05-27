418d48c101c952bc8074ff0b0ccaba8e31a9d139
personal <andresinostrozavalenzuela@gmail.com>
2022-05-27 12:44:12 +0200

    add coverage report

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -x
 python manage.py makemigrations api || exit 1
 python manage.py migrate || exit 1
-python manage.py test || exit 1
+coverage erase
+coverage run manage.py test || exit 1
+coverage report
 python manage.py loaddata organization
 python manage.py initsuperuser
 python manage.py runserver 0.0.0.0:8090
